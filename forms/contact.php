b60c7a9e78c2f259ccaeda6af8060ab87bc6ee42
Meet1903 <meet.diwan.md@gmail.com>
2023-11-17 23:21:58 -0500

    Added Project files

---
+++ forms/contact.php
@@ -0,0 +1,50 @@
+<?php
+  /**
+  * Requires the "PHP Email Form" library
+  * The "PHP Email Form" library is available only in the pro version of the template
+  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
+  * For more info and help: https://bootstrapmade.com/php-email-form/
+  */
+
+  // Replace contact@example.com with your real receiving email address
+  $receiving_email_address = 'cp1903md@gmail.com';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['name'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject = $_POST['subject'];
+
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
+  $contact->smtp = array(
+    'host' => 'example.com',
+    'username' => 'example',
+    'password' => 'pass',
+    'port' => '587'
+  );
+  */
+  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  $contact->smtp = array(
+  'host' => 'smtp.gmail.com', // Gmail SMTP server address
+  'username' => 'cp1903md@@gmail.com', // Your Gmail address
+  'password' => 'HelloThere@1903', // Your Gmail password or an App Password if you have 2-step verification enabled
+  'port' => '587', // Gmail SMTP port for TLS
+  'encryption' => 'tls' // Use 'tls' for Gmail
+  );
+
+
+  $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['email'], 'Email');
+  $contact->add_message( $_POST['message'], 'Message', 10);
+
+  echo $contact->send();
+?>
