dd2883d0dd1b0e6a7d835d733f52e5c089875731
Meet1903 <meet.diwan.md@gmail.com>
2024-07-06 01:49:33 -0400

    Added aos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Header from './components/Header'
 import TitlePage from './components/TitlePage';
+import About from './components/About';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import {useEffect} from 'react';
 
 function App() {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
     <div className="App">
       <Header />
       <TitlePage />
+      <main id="main">
+        <About />
+      </main>
     </div>
   );
 }
