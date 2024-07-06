f2c37e3facc50b7446551b0c840f7bcb621626fd
Meet1903 <meet.diwan.md@gmail.com>
2024-07-06 00:26:08 -0400

    Updated CSS for Title page.

---
+++ src/components/TitlePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TitlePage() {
+    return (
+        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="hero-container" data-aos="fade-in">
+            <h1>Meet Diwan</h1>
+            <p>I'm <span className="typed" data-typed-items="a Software Developer, an ML Engineer, a Philosophy Enthusiast, a Tennis Fan, an avid reader"></span></p>
+            </div>
+        </section>
+    )
+}
