1676a44035bef01032d55f5ce44adae180ca2eea
Meet1903 <meet.diwan.md@gmail.com>
2024-07-06 02:33:49 -0400

    Updated About.js content

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import DisplayImage from '../assets/img/Display.jpg'
+import AboutImage from '../assets/img/AboutPage.jpg'
 
 export default function About() {
     return (
@@ -8,11 +8,11 @@ export default function About() {
 
                 <div className="section-title">
                     <h2>About</h2>
-                    <p>Master's student at New York University.</p>
+                    <p></p> {/* May add it later*/}
                 </div>
                 <div className="row">
                     <div className="col-lg-4" data-aos="fade-right">
-                        <img src={DisplayImage} className="img-fluid" alt="" />
+                        <img src={AboutImage} className="img-fluid" alt="" />
                     </div>
                     <div className="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
                         <h3>Software Developer</h3>
@@ -28,16 +28,27 @@ export default function About() {
                             </div>
                             <div className="col-lg-6">
                                 <ul>
-                                <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Master's in Computer Engineering</span></li>
+                                <li><i className="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>MS in Computer Engineering</span></li>
                                 <li><i className="bi bi-chevron-right"></i> <strong>Email:</strong> <span><a href="mailto:meet.diwan@nyu.edu">meet.diwan@nyu.edu</a></span></li>
                                 <li><i className="bi bi-chevron-right"></i> <strong>Full time role:</strong> <span>Available from May 2024</span></li>
                                 </ul>
                             </div>
                         </div>
                         <p className="overview">
-                        Before opting for the Master's degree, I worked as a Software Engineer at one of the renowned & earliest Product based companies of India named
-                        Subex Limited for one year. I also have six months of internship experience at Crest Data Systems. These experiences have provided me with 
-                        the necessary skills to work effectively in a professional environment, collaborate with team members, and develop highly efficient and performant code.
+                        With around 2 years of experience as a Software Engineer, I possess a solid background in programming languages, data structures, algorithms, and software development. 
+                        My expertise extends to full-stack web application development, leveraging technologies such as Angular, React, TypeScript, JavaScript, and Java Spring Boot. Notably, 
+                        I have contributed to Subex's flagship product, HyperSense Business Management Studio, by creating various modules.
+                        <br />
+                        <br />
+                        I am a recent graduate with a Master's in Computer Engineering from NYU, I have also served as a Graduate Teaching Assistant 
+                        for a course on Data Analytics and Visualization in Healthcare. 
+                        <br />
+                        <br />
+                        As I embark on the next phase of my career journey, I am open to opportunities in Software Engineering, Data Analysis, and Machine Learning Engineering roles. 
+                        Eager to apply my diverse skill set to tackle new challenges, I am committed to contributing to the success of any organization I join. 
+                        <br />
+                        <br />
+                        Please feel free to reach out if you have any inquiries or would like to connect. I look forward to exploring potential opportunities where I can make meaningful contributions.
                         </p>
                     </div>
                 </div>
