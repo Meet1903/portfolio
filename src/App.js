0704e64e4c55a979f558405580513363d5df5fb1
Meet1903 <meet.diwan.md@gmail.com>
2024-07-06 00:25:39 -0400

    Added Title Page component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header'
+import TitlePage from './components/TitlePage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <TitlePage />
     </div>
   );
 }
