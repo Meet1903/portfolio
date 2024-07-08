8b71e040377cd20674f5523d310587b1ae1dda65
Meet1903 <meet.diwan.md@gmail.com>
2024-07-08 11:35:43 -0400

    Added toggle button for mobile site

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
 import TitlePage from './components/TitlePage';
 import About from './components/About';
 import Skills from './components/Skills';
 import Resume from './components/Resume';
-import ExtraCurricular from './components/ExtraCurricular'
+import ExtraCurricular from './components/ExtraCurricular';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
     AOS.init({ duration: 1000 });
+
+    const toggleMobileNav = (e) => {
+      document.body.classList.toggle('mobile-nav-active');
+      e.currentTarget.classList.toggle('bi-list');
+      e.currentTarget.classList.toggle('bi-x');
+    };
+
+    const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
+    if (mobileNavToggle) {
+      mobileNavToggle.addEventListener('click', toggleMobileNav);
+    }
+
+    return () => {
+      if (mobileNavToggle) {
+        mobileNavToggle.removeEventListener('click', toggleMobileNav);
+      }
+    };
   }, []);
+
   return (
     <div className="App">
+      <i className="bi bi-list mobile-nav-toggle d-xl-none"></i>
       <Header />
       <TitlePage />
       <main id="main">
