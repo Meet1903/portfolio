97e11714815f2d4f0baee6fb0ef924dc3bda3a71
Meet1903 <meet.diwan.md@gmail.com>
2024-07-08 10:49:13 -0400

    CSS issue resolved

--- src/components/TitlePage.js
+++ src/components/TitlePage.js
@@ -31,8 +31,8 @@ export default function TitlePage() {
     }, []);
 
     return (
-        <section id="hero" className="d-flex flex-column justify-content-center align-items-center">
-            <div className="hero-container" data-aos="fade-in">
+        <section id="home" className="d-flex flex-column justify-content-center align-items-center">
+            <div className="home-container" data-aos="fade-in">
                 <h1>Meet Diwan</h1>
                 <p>I'm <span ref={typedElement} className="typed" data-typed-items="a Software Developer, an ML Engineer, a Philosophy Enthusiast, a Tennis Fan, an avid reader"></span></p>
             </div>
